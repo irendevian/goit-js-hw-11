ce359fb9b7165c8cb57c4bd38d73d9396fe04838
Iryna Devianina <devianinairen@gmail.com>
2025-07-31 19:46:03 +0300

    start js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,3 +1,18 @@
-function getImagesByQuery(query) {
-    
+import axios from "axios";
+
+const MY_API_KEY = "51560534-ee0888e2507fe11f794ee77c8";
+
+
+ export function getImagesByQuery(query) {
+
+     return axios('https://pixabay.com/api/', {
+            params: {
+                key: MY_API_KEY,
+                q: query,
+                image_type: "photo",
+                orientation: "horizontal",
+                safesearch: true
+            }
+        });
 }
+
--- src/main.js
+++ src/main.js
@@ -1,6 +1,42 @@
-// import getImagesByQuery(query) from pixabay - api.js;
+import { getImagesByQuery } from './js/pixabay-api.js';
 // import createGallery(images) from render - functions.js;
 // import clearGallery() from render - functions.js;
 // import showLoader() from render - functions.js;
 // import hideLoader() from render - functions.js;
+
+
+const form = document.querySelector(".form");
+const input = document.querySelector("input");
+const gallery = document.querySelector(".gallery");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const userValueInput = input.value.trim();
+
+    if (!userValueInput) {
+        alert("Заповніть поле!");
+        return;
+    }
+
+    getImagesByQuery(userValueInput)
+    .then(res => {
+        const images = res.data.hits;
+
+        if (images.length === 0) {
+            alert("Sorry, there are no images matching your search query. Please try again!")
+        } else {
+            gallery.innerHTML = "";
+            gallery.insertAdjacentHTML("beforeend", createMarkup(images));
+        }
+             
+         })
+    .catch(error => {
+             console.log(error);
+             
+         })
+
+}
 
