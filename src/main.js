c7507d7d58d0cf601dd177314331397c861cc662
Iryna Devianina <devianinairen@gmail.com>
2025-07-31 15:33:28 +0300

    add css

---
+++ src/main.js
@@ -0,0 +1,6 @@
+// import getImagesByQuery(query) from pixabay - api.js;
+// import createGallery(images) from render - functions.js;
+// import clearGallery() from render - functions.js;
+// import showLoader() from render - functions.js;
+// import hideLoader() from render - functions.js;
+
